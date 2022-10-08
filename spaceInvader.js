e9864c1760e8c02d7f21db4c7fff657eff0f47bc
Safae Najjar <safae.najjari@student.ehb.be>
2022-10-08 23:14:38 +0200

    Assignment 1: git space invader safae najjari

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,121 @@
 "use strict";
 
-alert("Draw your space invader here");
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+ canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
+
+drawLine();
+
+function drawLine(){
+    
+    context.beginPath();
+    context.rect(15,15,300,300);
+    context.fillStyle="black";
+    context.fill();
+
+    context.beginPath();
+    context.rect(40,40,50,50);
+    context.fillStyle="lightgreen";
+    context.fill();
+
+    context.beginPath();
+    context.rect(40,90,50,50);
+    context.fillStyle="lightgreen";
+    context.fill();
+
+    context.beginPath();
+    context.rect(40,140,50,50);
+    context.fillStyle="lightgreen";
+    context.fill();
+
+    context.beginPath();
+    context.rect(40,190,50,50);
+    context.fillStyle="lightgreen";
+    context.fill();
+//
+    context.beginPath();
+    context.rect(90,40,50,50);
+    context.fillStyle="lightgreen";
+    context.fill();
+    
+
+    context.beginPath();
+    context.rect(140,40,50,50);
+    context.fillStyle="lightgreen";
+    context.fill();
+
+    context.beginPath();
+    context.rect(190,40,50,50);
+    context.fillStyle="lightgreen";
+    context.fill();
+
+    context.beginPath();
+    context.rect(240,40,50,50);
+    context.fillStyle="lightgreen";
+    context.fill();
+//
+    
+
+    context.beginPath();
+    context.rect(240,90,50,50);
+    context.fillStyle="lightgreen";
+    context.fill();
+
+    context.beginPath();
+    context.rect(240,140,50,50);
+    context.fillStyle="lightgreen";
+    context.fill();
+
+    context.beginPath();
+    context.rect(240,190,50,50);
+    context.fillStyle="lightgreen";
+    context.fill();
+//
+
+    context.beginPath();
+    context.rect(140,90,50,50);
+    context.fillStyle="lightgreen";
+    context.fill();
+
+//
+    context.beginPath();
+    context.rect(90,140,50,50);
+    context.fillStyle="lightgreen";
+    context.fill();
+//
+    context.beginPath();
+    context.rect(190,140,50,50);
+    context.fillStyle="lightgreen";
+    context.fill();
+//
+
+    context.beginPath();
+    context.rect(90,190,50,50);
+    context.fillStyle="lightgreen";
+    context.fill();
+
+//
+    context.beginPath();
+    context.rect(190,190,50,50);
+    context.fillStyle="lightgreen";
+    context.fill();
+
+//
+
+    context.beginPath();
+    context.rect(90,240,50,50);
+    context.fillStyle="lightgreen";
+    context.fill();
+
+    context.beginPath();
+    context.rect(140,240,50,50);
+    context.fillStyle="lightgreen";
+    context.fill();
+
+    context.beginPath();
+    context.rect(190,240,50,50);
+    context.fillStyle="lightgreen";
+    context.fill();
+}
+
